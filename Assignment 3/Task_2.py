3ed24796b1fbd8c0f7cb0723d685b1697902c27e
Gautam Bhushan <148893478+G-for-Gautam@users.noreply.github.com>
2025-04-06 15:56:46 +0530

    Create Task_2.py

---
+++ Assignment 3/Task_2.py
@@ -0,0 +1,6 @@
+import math
+
+num=int(input('enter a number: '))
+print('it\'s square root is = ',math.sqrt(num))
+print('it\'s logrithmic value is = ',math.log(num))
+print('it\'s sine value is = ',math.sin(num))
